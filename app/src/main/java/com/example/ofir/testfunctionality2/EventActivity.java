689c6fa5d2f3a5e19387a1d131e5800025e55798
Alonka <al8389@gmail.com>
2016-11-13 10:06:56 +0200

    tryyy

--- app/src/main/java/com/example/ofir/testfunctionality2/EventActivity.java
+++ app/src/main/java/com/example/ofir/testfunctionality2/EventActivity.java
@@ -16,7 +16,7 @@ public class EventActivity extends AppCompatActivity {
     TextView getACKStatus;
     TextView getDateAndTime;
     TextView getMaxNumOfParticipants;
-//////alona123
+//////alona12345
 
     public static final String MY_PREFS_NAME = "MyPrefsFile";
 
